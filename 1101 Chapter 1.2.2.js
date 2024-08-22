1ecddec9883b8fc926fa96dc99923111c6e843c3
Source Academy User <No public email provided>
2024-08-22 16:40:05 +0800

    Changes made from Source Academy

---
+++ 1101 Chapter 1.2.2.js
@@ -0,0 +1,17 @@
+// Function with recursive call will give u either recurssive or iterative processes
+
+
+// function g(x) {
+//     return (x === 0) ? 0 : 1 + g(x - 1);
+// }
+// recursive process will give the distribute of accumulation of deffered equations, in the function 1+ g(x-1) will produce deferred equations.
+
+// (ii) function f(x, y) {
+//     return (x === 0) ? y : f(x - 1, y + 1);
+// (a) iterative process will evaluation each step first before going to the next step of evaluation, f(x-1, x+1) will be evaluate first at each function call before going to the other.
+   (b) 
+
+// (iii) in computer operation, the time of program operation can be measured the steps of operation which are undertaken by the computer.
+3.5 % 2;
+
+// (iv) 
