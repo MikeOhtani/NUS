bf847ea02b182485d4a9ec0cf799bbfaaff25a33
Source Academy User <No public email provided>
2024-08-26 02:21:35 +0800

    Changes made from Source Academy

--- 1101.studio_3.js
+++ 1101.studio_3.js
@@ -7,7 +7,32 @@ import { blank, square, show, heart, beside, stack} from 'rune';
 // }
 
 // show(f1(square, 3, heart));
+Q1: 
+function f1(rune1, rune2) {
+    stack(rune1, rune2);
+}
 
+function f2(rune1, rune2) {
+    beside(square,f1(rune1, rune2));
+}
+
+function f3(rune1, rune2) {
+    stack(blank,f2(rune1, rune2));
+}
+
+function f4(rune1, rune2) {
+    beside(square,f3(rune1, rune2));
+}
+
+function f5(rune1, rune2) {
+    stack(blank, f4(rune1, rune2));
+}
+
+function f6(rune1, rune2) {
+    beside(square, f5(rune1, rune2));
+}
+
+show(f1(blank, heart));
 
 // Use the substitution model on runes demonstrated during Lecture L2A in order to manually
 // evaluate the expression f1(square, 3, heart). The evaluation proceeds as demonstrated
@@ -18,23 +43,25 @@ import { blank, square, show, heart, beside, stack} from 'rune';
 // draw the pictures as large as necessary.
 
 
-function f1(rune_1, rune_2, rune_3) {
-    return beside(rune_1, stack(rune_2, rune_3));
-}
+// Q2: 
 
-function f2(rune_1, rune_2, rune_3) {
-    return beside(rune_1, stack(rune_2, f1(rune_1, rune_2, rune_3)));
-}
+// function f1(rune_1, rune_2, rune_3) {
+//     return beside(rune_1, stack(rune_2, rune_3));
+// }
 
-function f3(rune_1, rune_2, rune_3) {
-    return beside(rune_1, stack(rune_2, f2(rune_1, rune_2, rune_3)));
-}
+// function f2(rune_1, rune_2, rune_3) {
+//     return beside(rune_1, stack(rune_2, f1(rune_1, rune_2, rune_3)));
+// }
 
-function f4(rune_1, rune_2, rune_3) {
-    return beside(rune_1, stack(rune_2, f3(rune_1, rune_2, rune_3)));    
-}
+// function f3(rune_1, rune_2, rune_3) {
+//     return beside(rune_1, stack(rune_2, f2(rune_1, rune_2, rune_3)));
+// }
 
-function f5(rune_1, rune_2, rune_3) {
-    return beside(rune_1, stack(rune_2, f4(rune_1, rune_2, rune_3)));    
-}
-show(f3(square, blank, heart));
+// function f4(rune_1, rune_2, rune_3) {
+//     return beside(rune_1, stack(rune_2, f3(rune_1, rune_2, rune_3)));    
+// }
+
+// function f5(rune_1, rune_2, rune_3) {
+//     return beside(rune_1, stack(rune_2, f4(rune_1, rune_2, rune_3)));    
+// }
+// show(f3(square, blank, heart));
