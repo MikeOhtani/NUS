1951b76f2c040dd6552d7c11a909999352513d28
Source Academy User <No public email provided>
2024-08-29 21:16:39 +0800

    Changes made from Source Academy

--- 1101_reflection3.js
+++ 1101_reflection3.js
@@ -11,3 +11,28 @@
 
 // deffered functions means how many operations need to be executed ; e,g 1+2+sum(3), 2 deffered
 
+// Iterative process
+
+
+
+function sum(term, a, next, b) {
+    return (a > b) 
+           ? 0
+           : term(a) + sum(term, next(a), next, b);
+}
+
+function iter(term, a, next, b, acc) {
+    return a > b
+           ? acc
+           : iter(term, next(a), next, b, acc + term(a));
+}
+
+function sum_iter(term, a, next, b) {
+    return iter(term, a, next, b, 0);
+}
+
+function my_sum2(n) { 
+    return sum_iter(x=> x * (x + 1), 1, x=> x + 1, n);
+}
+
+my_sum2(2);
