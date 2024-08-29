47ac4ed0cea95b32ec654572c88d0b7b0d0a062d
Source Academy User <No public email provided>
2024-08-29 15:28:02 +0800

    Changes made from Source Academy

---
+++ 1101_reflection3.js
@@ -0,0 +1,13 @@
+// const z = 1; // commend + shift + h : highlight the whole function
+
+// function f(g) {
+//     const z = 3;
+//     return g(z);
+// }
+
+// f(y => y + z);
+
+// // commend + B to the declaration of the Z.
+
+// deffered functions means how many operations need to be executed ; e,g 1+2+sum(3), 2 deffered
+
