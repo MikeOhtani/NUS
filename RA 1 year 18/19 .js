94449e5b64548b506a91a6c436a7600224b1d436
Source Academy User <No public email provided>
2024-09-06 15:08:24 +0800

    Changes made from Source Academy

--- RA 1 year 18/19 .js
+++ RA 1 year 18/19 .js
@@ -1,51 +1,10 @@
-// Q 7 
-//     function twice(f) {
-//     return x => f(f(x));
-// }
-// (twice(twice))(x => 2 * x + 1)(2);
-
-// Q12
-function f(n) {
-    if (n <= 0) {
-        return true;
-    } else {
-        return true && f(n - 1);
-    }
+function foo(x, y) {
+    return x === 0
+         ? y === 0
+         ? 1
+         : foo(x, y - 1)
+         : foo(x - 1, y);
 }
-/* true && : left operator evalurate sucessfully
-as it is a true boolean value and continue with 
-the right operator evaluation which depends on 
-whether 
 
-f(10);
 
-// Q13
-// function E(x) {
-//     if (x === 1) {
-//         return false;
-//     } else if (x === 2) {
-//         return true;
-//     } else {
-//         return E(x - 2);
-//     }
-// }
-
-// E(1);
-
-// Q 14 
-// function M(a, b, c) {
-//     if (a < b) {
-//         return (a < c) ? a : c;
-//     } else {
-//         return (b < c) ? b : c;
-//     }
-// }
-// M(1, 2, 3);
-
-// Q 15
-// function X(a, b) {
-//     return (a !== b);
-// }
-
-// X("false", "true");
-
+foo(0, 2);
